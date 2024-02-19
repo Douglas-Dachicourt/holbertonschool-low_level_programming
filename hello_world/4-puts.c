1d241e06fbd68c1635db6fb9ab2f335d1166e815
Douglas Dachicourt <8815@holbertonstudents.com>
2024-02-19 16:22:26 +0000

    try

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-
-/*début du code*/ 
-
-int main(void) /*Fonction main obligatoire*/
+/*début du code*/
+/*Fonction main obligatoire*/
+int main(void) 
 {
+	puts("\"Programming is like building a multilingual puzzle");
 
-	puts("\"Programming is like building a multilingual puzzle\n");
-
-return (0);
-
+	return (0);
 }
