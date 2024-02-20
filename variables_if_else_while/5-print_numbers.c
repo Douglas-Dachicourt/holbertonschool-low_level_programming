269bd4083473dcd470c4bfc12bd0785a755a8407
Douglas Dachicourt <8815@holbertonstudents.com>
2024-02-20 10:06:46 +0000

    ajout file 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - fonction obligatoire
+ *
+ * Return: 0 si tout se passe bien
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+		putchar('\n');
+
+	return (0);
+}
