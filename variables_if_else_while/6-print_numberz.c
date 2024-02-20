e41a1e76284c84f411119e8abd37393271d079a9
Douglas Dachicourt <8815@holbertonstudents.com>
2024-02-20 10:13:25 +0000

    ajout file 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - fonction obligatoire
+ *
+ *Return: 0 si tout se passe bien
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
