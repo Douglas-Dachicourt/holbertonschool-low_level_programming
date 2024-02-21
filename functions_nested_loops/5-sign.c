415588211bdd42fb41cbac20708338ed36df0aee
Douglas Dachicourt <8815@holbertonstudents.com>
2024-02-21 13:41:37 +0000

    ajout file 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign - indique le signe du chiffre teste
+ * @n: le chiffre teste
+ *
+ * Return: 1 si n>0, 0 si n=0, 0 si n<0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+		return (0);
+}
