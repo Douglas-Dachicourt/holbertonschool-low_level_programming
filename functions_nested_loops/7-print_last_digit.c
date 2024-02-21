8c637493e47484db9d7f9818781d675339f25039
Douglas Dachicourt <8815@holbertonstudents.com>
2024-02-21 16:16:33 +0000

    ajout file 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * print_last_digit - imprime le dernier chiffre de l'entier
+ * @r: parametre a tester
+ *
+ * Return: 0 si tout se déroule bien
+ */
+int print_last_digit(int r)
+{
+	int d;
+
+	if (r > 0)
+	{
+		d = r % 10;
+		return _putchar('0' + d);
+	}
+	if (r == 0)
+	{
+		return _putchar('0');
+	}
+	if (r < 0)
+	{
+		r = -r;
+		d = r % 10;
+		return _putchar('0' + d);
+	} else
+	{
+		d = r % 10;
+		return _putchar('0' + d);
+	}
+
+	return (0);
+}
+
+
+
