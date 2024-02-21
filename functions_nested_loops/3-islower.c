39325889d6515df8d999e485b30419583b06fad0
Douglas Dachicourt <8815@holbertonstudents.com>
2024-02-21 13:12:21 +0000

    try

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -5,9 +5,10 @@
  * Return: 1 si c est lowercase
  * sinon return 0
  */
- int _islower(int c)
- {
+int _islower(int c)
+{
 	if (c >= 97 && c <= 122)
 		return 1;
-	else return 0;
- }
+	else
+		return 0;
+}
