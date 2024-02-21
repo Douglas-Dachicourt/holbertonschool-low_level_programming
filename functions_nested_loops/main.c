ba76a68661edd8a6816ced170555c3f3657af554
Douglas Dachicourt <8815@holbertonstudents.com>
2024-02-21 10:11:34 +0000

    ajout main.c

---
+++ functions_nested_loops/main.c
@@ -0,0 +1,15 @@
+int _putchar(char c);
+/**
+ * print_alphabet - print l'alphabet
+ *
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
