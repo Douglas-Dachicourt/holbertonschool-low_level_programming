d0d8b31a534c7cae516e9653a321a52da75a2558
Douglas Dachicourt <8815@holbertonstudents.com>
2024-02-21 09:12:36 +0000

    try

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,21 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * main - fonction obligatoire
  *
- * Return: 0 si tout va bien
+ * Return: 0 si tout se passe bien
  */
 int main(void)
 {
-	_putchar();
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
