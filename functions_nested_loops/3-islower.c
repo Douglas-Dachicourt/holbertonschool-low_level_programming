c7c12c1dd5d991f04832af018898edaa397521df
Douglas Dachicourt <8815@holbertonstudents.com>
2024-02-21 13:10:34 +0000

    ajout de la file 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * islower - fonction
+ *
+ * Return: 1 si c est lowercase
+ * sinon return 0
+ */
+ int _islower(int c)
+ {
+	if (c >= 97 && c <= 122)
+		return 1;
+	else return 0;
+ }
