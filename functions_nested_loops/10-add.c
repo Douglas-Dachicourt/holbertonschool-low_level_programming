bd59fe90e2d565933939196296761e1cf2db56d1
Douglas Dachicourt <8815@holbertonstudents.com>
2024-02-22 12:32:46 +0000

    ajout file 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - fonction qui additionne deux entiers entre eux
+ * @a: valeur nombre un
+ * @b: valeur nombre 2
+ *
+ * Return: la somme
+ **/
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
