f7cfb1beac5a71474aae666b4c0f019c0284cb27
Douglas Dachicourt <8815@holbertonstudents.com>
2024-02-22 12:50:59 +0000

    try

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - imprime les nombre de n à 98
+ * @n: entier de départ n choisi par l'utilisateur
+ *
+ * Return: 0 si tout se déroule bien
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		printf("%d", i);
+		printf(",");
+		printf(" ");
+    }
+	printf("\n");
+}
