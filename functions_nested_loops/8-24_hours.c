44055683f3cd738173cec3522578e1e6c6e3b6ea
Douglas Dachicourt <8815@holbertonstudents.com>
2024-02-22 17:06:16 +0000

    ajout file 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,6 +1,22 @@
 #include "main.h"
 /**
- *
+ * jack_baueur - fonction qui énumère les minutes en 24H
  *
  *
  */
+void jack_bauer(void)
+{
+int i;
+char c;
+
+for (i = 0; i < 60; i++)
+{
+	_putchar('\n');
+
+	for (c = 48; c <= 53; c++)
+	{
+		_putchar(c);
+	}
+}
+
+}
