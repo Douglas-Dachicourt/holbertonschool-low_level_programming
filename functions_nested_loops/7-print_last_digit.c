5399f3e964ce6dfc8eeaaf0d0d07a96a3ba5f94b
Douglas Dachicourt <8815@holbertonstudents.com>
2024-02-22 08:27:32 +0000

    try

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "6-abs.c"
 /**
  * print_last_digit - imprime le dernier chiffre de l'entier
  * @n: parametre a tester
@@ -7,7 +8,26 @@
  */
 int print_last_digit(int n)
 {
-	int d, e, result;
+	int d, result;
+
+	d = _abs(n) % 10;
+
+	if (n >= 0)
+	{
+		result = _putchar('0' + d);
+		return (result);
+	}
+	else
+	{
+		result = _putchar('0' + d);
+	}
+	return (d);
+}
+
+
+/**
+char d, e;
+	int result;
 
 	if (n >= 0)
 	{
@@ -20,7 +40,6 @@ int print_last_digit(int n)
 		n = -(n);
 		e = n % 10;
 		result = _putchar('0' + e);
-		return (result);
 	}
-	return (0);
-}
+
+*/
