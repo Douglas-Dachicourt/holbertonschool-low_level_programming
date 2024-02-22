621d889d1acb313148c0962ed14dbec708841860
Douglas Dachicourt <8815@holbertonstudents.com>
2024-02-22 13:34:30 +0000

    try

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -7,13 +7,11 @@
  */
 int main(void)
 {
-
     print_to_98(0);
     print_to_98(98);
     print_to_98(111);
     print_to_98(81);
     print_to_98(-10);
-	print_to_98(-111);
-
+	print_to_98(-98);
     return (0);
 }
