7b6d9ce0c27988f7470fd4a345e3803eb1d094ba
Douglas Dachicourt <8815@holbertonstudents.com>
2024-02-23 10:37:25 +0000

    try

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * times_table - fonction multiply tables
+ */
+void times_table(void)
+{
+int i, j;
+
+for (i = 0; i <= 9; i++)
+{
+	for (j = 0; j <= 9; j++)
+	{
+		int m = i * j, c = m / 10, d = m % 10;
+
+		if(m > 9)
+		_putchar('0' + c);
+		_putchar('0' + d);
+
+		if (j < 9)
+		_putchar(',');
+
+	    if ((i == 2 && j < 4) || (i == 3 && j < 3)
+		|| ((i == 0 || i == 1) && (j < 9)) || (i == 4 && j < 2))
+		{
+			_putchar(' ');
+			_putchar(' ');
+		} else if ((i == 2 && j >= 4) || (i == 3 && j >= 3)
+		|| (i == 4 && j >= 2))
+		{
+			_putchar(' ');
+		}
+		else
+		{
+			if (j < 1)
+			{
+				_putchar(' ');
+			    _putchar(' ');
+			} else if (j >= 1)
+				_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
+}
