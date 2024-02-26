a968f629646949ca2ddd7ecc940921c810838a31
Douglas Dachicourt <8815@holbertonstudents.com>
2024-02-26 13:41:59 +0000

    try

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,10 +10,12 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) && (i % 5) == 0){
+		if ((i % 3 == 0) && (i % 5) == 0)
+		{
 			printf("FizzBuzz");
 			printf(" ");
-		} else
+		}
+		else
 		{
 			if (i % 3 == 0)
 			{
