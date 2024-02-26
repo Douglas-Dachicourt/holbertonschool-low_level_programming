2225bebf1287affc97ddb2d902cb4bcf5150144b
Douglas Dachicourt <8815@holbertonstudents.com>
2024-02-26 12:37:01 +0000

    try

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -10,12 +10,11 @@ void print_square(int size)
 
 	for (i = 0; i < size; i++)
 	{
-		if (size == 0 || size < 0)
-		{
-			_putchar('\n');
-		}
 		for (j = 0; j < size; j++)
 		{
+			if (size == 0 || size < 0)
+				_putchar('\n');
+
 			_putchar('#');
 		}
 	_putchar('\n');
