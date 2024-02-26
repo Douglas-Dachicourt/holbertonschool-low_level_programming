f5c6e6a442cc52cd19305ae5c1e011c52af69252
Douglas Dachicourt <8815@holbertonstudents.com>
2024-02-26 09:02:02 +0000

    ajout file 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - fct qui multiplie les deux parametres entre eux
+ * @a: parametre 1
+ * @b: parametre 2
+ *
+ * Return: le résultat de la multiplication
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
