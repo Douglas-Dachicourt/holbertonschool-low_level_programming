b514df9f86dd99427ed588e2966241dac3105010
Douglas Dachicourt <8815@holbertonstudents.com>
2024-02-26 12:35:01 +0000

    ajout file 8

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_diagonal -
+ *
+ *
+ *
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for(j = 0; j < n; j++)
+		{
+
+		}
+	_putchar('\n');
+	}
+}
---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - print autant de square en hor et en vert avec char #
+ * @size: parametre
+ *
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		if (size == 0 || size < 0)
+		{
+			_putchar('\n');
+		}
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+	_putchar('\n');
+	}
+}
