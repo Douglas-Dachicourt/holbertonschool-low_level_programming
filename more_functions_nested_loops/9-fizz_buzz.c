a395553942db7d57cdba93651bd9d174cb02c916
Douglas Dachicourt <8815@holbertonstudents.com>
2024-02-26 13:04:08 +0000

    ajout file 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - fonction main
+ *
+ * Return: 0 si tout se passe bien
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+			printf(" ");
+		} else if (i % 5 == 0)
+		{
+			printf("Buzz");
+			printf(" ");
+		} else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("Buzz");
+			printf(" ");
+		} else
+		{
+			printf("%d", i);
+			printf(" ");
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
