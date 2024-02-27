0dd2ae18383be7e527d81256cc0f5212ea6b1621
Douglas Dachicourt <8815@holbertonstudents.com>
2024-02-27 17:41:17 +0000

    add file 6

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * rev_string -
+ * @s:
+ *
+ */
+void rev_string(char *s)
+{
+
+
+}
---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * puts2 - fct qui imprime un caractere sur deux
+ * @str: string parametre a tester
+ *
+ */
+void puts2(char *str)
+{
+	int i;
+
+	int l = _strlen(str);
+
+	for (i = 0; i <= l; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(*(str + i));
+		}
+	}
+_putchar('\n');
+}
