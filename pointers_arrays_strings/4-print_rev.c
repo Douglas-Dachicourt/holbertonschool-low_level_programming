f1b49c7b59e8dabc736239832b2a151c2427db8e
Douglas Dachicourt <8815@holbertonstudents.com>
2024-02-27 15:03:15 +0000

    try

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,9 +1,8 @@
 #include "main.h"
 #include "2-strlen.c"
-#include <stdio.h>
 /**
- * print_rev -
- * @s:
+ * print_rev - imprimer une string a l'envers
+ * @s: parametre teste
  *
  */
 void print_rev(char *s)
@@ -11,7 +10,7 @@ void print_rev(char *s)
 	int l = _strlen(s);
 	int i;
 
-	for (i = l-1; i >= 0; i--)
+	for (i = (l - 1); i >= 0; i--)
 	{
 		_putchar(*(s + i));
 	}
