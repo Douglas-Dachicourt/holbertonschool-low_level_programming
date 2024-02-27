e1393cc74c16da8ba222bd652b884c2b9f5bdb2d
Douglas Dachicourt <8815@holbertonstudents.com>
2024-02-27 10:31:22 +0000

    try

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
  * swap_int - interchanger deux valeurs initiale via les pointeurs
+ * @a: premier parametre
+ * @b: deuxieme parametre
  *
  */
 void swap_int(int *a, int *b)
