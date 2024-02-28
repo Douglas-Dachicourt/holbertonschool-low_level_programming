ab472252dcb4b33268b9056424f391ab5fbfb959
Douglas Dachicourt <8815@holbertonstudents.com>
2024-02-28 13:48:54 +0000

    try

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include "2-strlen.c"
-#include "_putchar.c"
 /**
  * _strcpy - fct qui recopie la string
  * @dest: pointeur de retour pour la copie recherchée
  * @src: source initiale à copier
+ * Return: dest ; la valeur pointée de retour qui est la copie de src
  */
 char *_strcpy(char *dest, char *src)
 {
@@ -13,9 +13,7 @@ char *_strcpy(char *dest, char *src)
 
 	for (i = 0; i <= l; i++)
 	{
-		dest = src;
-
-		_putchar(*(dest + i));
+		*(dest + i) = *(src + i);
 	}
 return (dest);
 }
