5d87ae7952bf243a48b570783b43fa6790c4162b
Douglas Dachicourt <8815@holbertonstudents.com>
2024-02-28 09:35:16 +0000

    try

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,19 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_array -
- *
- *
+ * print_array - fct qui retour les elements d'un tableau donné
+ *@a: parametre tableau a envoyer
+ *@n: nb d'éléments du tableau
  */
 void print_array(int *a, int n)
 {
 	int i;
 
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", *(a + i));
 
-		if(i < n - 1)
+		if (i < n - 1)
 		{
 			printf(", ");
 		}
