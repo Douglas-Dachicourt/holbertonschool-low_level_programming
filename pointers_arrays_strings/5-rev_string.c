a0375c02877fe12bde8c9f41e1fc0ea62f1460ae
Douglas Dachicourt <8815@holbertonstudents.com>
2024-02-28 12:47:20 +0000

    try

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,22 @@
 #include "main.h"
+#include "2-strlen.c"
 /**
- * rev_string -
- * @s:
+ * rev_string - fct qui inverse la string
+ * @s: string a tester
  *
  */
 void rev_string(char *s)
 {
+	int i, j;
+	int l = _strlen(s);
+	char temp;
 
-
+		for (i = 0, j = l - 1; i < j; i++, j--)
+		{
+			{
+				temp =  *(s + i);
+				*(s + i) = *(s + j);
+				*(s + j) = temp;
+			}
+		}
 }
---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "3-puts.c"
+#include "2-strlen.c"
+#include "_putchar.c"
+/**
+ * _strcpy -
+ * @dest:
+ * @src:
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int l = _strlen(src);
+
+	for (i = 0; i <= l; i++)
+	{
+
+	}
+return 0;
+}
