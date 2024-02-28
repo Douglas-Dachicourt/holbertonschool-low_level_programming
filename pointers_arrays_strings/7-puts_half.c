9a7b8fbbae7f43753ee39d7112963de0e7d31d58
Douglas Dachicourt <8815@holbertonstudents.com>
2024-02-28 09:19:21 +0000

    try

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include "2-strlen.c"
 /**
- * puts_half -
- * @str:
+ * puts_half - fct qui retourne la seconde moitié de string
+ * @str: string parametre a tester
  *
  */
 void puts_half(char *str)
@@ -10,10 +10,12 @@ void puts_half(char *str)
 	int l = _strlen(str);
 	int i;
 	int n = l / 2;
+	int m = l % 2;
 
 	for (i = n; i <= l; i++)
 	{
-		_putchar(*(str + i));
+		if (m == 0)
+			_putchar(*(str + i));
 	}
 _putchar('\n');
 }
