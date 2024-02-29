a20d2ba4362fafdd3360314aef8f646c0a14d84e
Douglas Dachicourt <8815@holbertonstudents.com>
2024-02-29 04:41:07 +0000

    try

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "2-strlen.c"
+#include "_putchar.c"
 /**
  * _atoi - fct qui transforme une chaine de caractere en entier
  * @s: string à convertir
@@ -11,13 +12,29 @@ int _atoi(char *s)
 	int i;
 	int n = 0;
 	int l = _strlen(s);
+	int signe;
+	int p = 0;
+	int m = 0;
 
 	for (i = 0; i <= (l - 1); i++)
 	{
-		if (s[i] >= '0' && s[i] <= '9')
+		if (s[i] == '+')
+		{
+			p++;
+		} else if (s[i] == '-')
+		{
+			m++;
+		}
+		if (p > m || p == m){
+			signe = 1;
+		} else
+		{
+			signe = -1;
+		}
+		if (s[i] >= '0' && s[i] <= '9' && s[i] != ' ')
 		{
 			n = n * 10 + (s[i] - '0');
 		}
 	}
-return (n);
+return (signe * n);
 }
