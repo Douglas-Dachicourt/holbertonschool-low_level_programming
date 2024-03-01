d522293480b8d9ae8b0b1afbad645df126777890
Douglas Dachicourt <8815@holbertonstudents.com>
2024-03-01 12:39:56 +0000

    ajout file 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * _strncpy - fct qui copie
+ * @dest: chaine de destination ou l'on copie la source
+ * @src: la source à copier
+ * @n: le nombre de caractère à copier
+ *
+ * Return: dest - chaine de destination
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+/*je parcoure la source jusqu'à n (nb de char voulus) ou jusqu'au char nul*/
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+/*je copie dans dest les char souhaités (soit jusqu'à n)*/
+		dest[i] = src[i];
+	}
+/*si la longueur de la source est < à n, je termine la chaine avec le '\0'*/
+		if (i < n)
+			dest[i] = '\0';
+
+return (dest);
+}
