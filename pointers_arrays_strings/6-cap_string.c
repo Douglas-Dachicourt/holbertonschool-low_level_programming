277ee368e41b15ddf62ac9cd44f27b2bec7cee38
Douglas Dachicourt <8815@holbertonstudents.com>
2024-03-01 21:29:07 +0000

    ajout file 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,18 +1,31 @@
 #include "main.h"
+#include "5-string_toupper.c"
 /**
- * cap_string -
- * @s:
+ * cap_string - fct qui met chaque premiere lettre d'un mot en majuscules
+ * @s: la chane de caractère à traduire
  *
- *Return: s
+ *Return: s = le résultat souhaité
  */
 char *cap_string(char *s)
 {
 	int i;
 
-
 	for (i = 0; s[i] != '\0'; i++)
 	{
-
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			if (s[i - 1] == ' ' || s[i - 1] == '.' || s[i - 1] == '\n' ||
+			s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '?' ||
+			s[i - 1] == '!' || s[i - 1] == '"' || s[i - 1] == '(' ||
+			s[i - 1] == ')' || s[i - 1] == '{' || s[i - 1] == '}')
+			{
+				s[i] -= 32;
+			} else if (s[i] >= 97 && s[i] <= 122 && s[i - 1] == '\t')
+			{
+			s[i - 1] = ' ';
+			s[i] -= 32;
+			}
+		}
 	}
 	return (s);
 }
