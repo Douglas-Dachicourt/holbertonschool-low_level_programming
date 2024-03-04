feb81ffa7e931f11fce2a4d66bf2c5425e970273
Douglas Dachicourt <8815@holbertonstudents.com>
2024-03-04 18:25:16 +0000

    ajout file 5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,11 +1,25 @@
 #include "main.h"
+#include <stddef.h>
 /**
- * _strstr -
- * @haystack:
- * @needle:
- *
+ * _strstr - fct qui recopie les caractères communs entre deux chaines
+ * @haystack: la chaine de base
+ * @needle: la chaine comparée à celle de base. On y copie les char présents
+ * Return: la résultante needle
  */
 char *_strstr(char *haystack, char *needle)
 {
+	int i, j;
 
+	for (i = 0; needle[i] != '\0'; i++)
+	{
+		for (j = 0; haystack[j] != '\0'; j++)
+		{
+			if (needle[i] == haystack[j])
+			{
+				return (&needle[i]);
+			}
+		}
+		return (NULL);
+	}
+	return (needle);
 }
