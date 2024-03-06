c72909a3ba5528150f69f6819b1369330734f9d6
Douglas Dachicourt <8815@holbertonstudents.com>
2024-03-06 15:27:29 +0000

    ajout file 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,31 +1,31 @@
 #include "main.h"
 /**
- *
- *
- *
- *
+ * prime - checker si n est premier ou non/ verifier les diviseurs
+ * @a: le diviseur à tester sur n
+ * @n: le nb testé
+ * Return: vérification du prochain diviseur (incrémentation de a)
+ */
+int prime(int a, int n)
+{
+	if (n % a == 0)
+	{
+		return (0);
+	} else if (a * a > n)
+	{
+		return (1);
+	}
+return (prime(a + 1, n));
+}
+/**
+ * is_prime_number - fct qui affiche le résultat de la précedente fct
+ * @n: le nb testé
+ * Return: le résultat , 1 si n est premier, 0 dans l'autre cas
  */
 int is_prime_number(int n)
 {
-	if (n > 1 && ((n % 2) != 0))
-	{
-		return (1);
-	} else if (n < 0)
+	if (n <= 1)
 	{
 		return (0);
 	}
-return is_prime_number(n - 1);
+return (prime(2, n));
 }
-
-
-
-
-
-
-/*
- * si n/n et n / 1 =
- *
- *
- *
- *
- */
