09859ba223956690156eb02e0691fe5d9a129be8
Douglas Dachicourt <8815@holbertonstudents.com>
2024-03-06 08:13:20 +0000

    ajout file 2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -6,10 +6,12 @@
  */
 int _strlen_recursion(char *s)
 {
+	int len = 0;
+
 	if (*s == '\0')
 	{
-		return 0;
+		return (0);
 	}
-
-	return _strlen_recursion(s + 1);
+	len++;
+	return (len + _strlen_recursion(s + 1));
 }
