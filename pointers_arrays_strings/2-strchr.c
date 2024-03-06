e229addef86d5159061071ad5388b40648e61001
Douglas Dachicourt <8815@holbertonstudents.com>
2024-03-06 16:46:42 +0000

    try

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -8,17 +8,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i] != '\0')
 	{
 		if (s[i] == c)
-		{
 			return (&s[i]);
-		} else
-		{
-			return (NULL);
-		}
+
+		i++;
 	}
-	return NULL;
+	return (NULL);
 }
