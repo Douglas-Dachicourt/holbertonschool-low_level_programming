89b31c5f4576a24b15c63a8ba767773087899e62
Douglas Dachicourt <8815@holbertonstudents.com>
2024-03-07 19:29:30 +0000

    ajout file 4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - fct qui
+ * @argc: nb d'arguments
+ * @argv: tableau d'éléments
+ * Return: exit code status 0 si tout se passe bien
+ */
+int main(int argc, char *argv[])
+{
+	int i, num, sum = 0;
+
+	if (!argv[0])
+	{
+		printf("0\n");
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		num = atoi(argv[i]);
+		sum = sum + num;
+	}
+
+	if (!num)
+	{
+		printf("Error\n");
+	return (1);
+	}
+
+	printf("%d\n", sum);
+return (0);
+}
