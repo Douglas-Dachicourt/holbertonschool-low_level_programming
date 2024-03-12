6ae57316d4fde95e93a59a2d31c38ce35cea97d9
Douglas Dachicourt <8815@holbertonstudents.com>
2024-03-12 15:52:19 +0000

    try

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * malloc_checked - fct qui alloue de l'espace à une donnée, return 98 si fail
+ * malloc_checked - fct qui alloue de l'espace à une donnée, ret 98 si fail
  * @b: parametre d'allocation
  * Return: un pointeur vers le nouvel espace alloué
  */
@@ -9,11 +9,9 @@ void *malloc_checked(unsigned int b)
 {
 	void *space;
 
-	space = malloc((sizeof(unsigned int) * b) + 1);
+	space = malloc(b);
 
 	if (space == NULL)
-	{
-		exit (98);
-	}
+		exit(98);
 	return (space);
 }
