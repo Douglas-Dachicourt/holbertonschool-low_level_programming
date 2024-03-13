9a9ee2f1d0b0896e3d8997587199bee24a9155f4
Douglas Dachicourt <8815@holbertonstudents.com>
2024-03-13 12:51:31 +0000

    ajout file 3

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * array_range - créer un tableau pour y intégrer des int (min à max)
+ * @min: int minimum
+ * @max: int maximum
+ * Return: le pointeur vers le tableau crée
+ */
+int *array_range(int min, int max)
+{
+	int *array;
+	int i;
+
+	if (min > max)
+		return (NULL);
+
+	array = malloc((max - min) * sizeof(int));
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = min; i <= max; i++)
+	{
+		array[i] = i;
+	}
+return (array);
+}
