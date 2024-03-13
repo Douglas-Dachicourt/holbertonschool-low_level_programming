cfd2dcd86e3f129ef0e1c79f4417434b6f95dc50
Douglas Dachicourt <8815@holbertonstudents.com>
2024-03-13 13:24:40 +0000

    try

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -14,15 +14,14 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	array = malloc((sizeof(int) * 2) * (max - min));
+	array = malloc(sizeof(int) * (max - min + 1));
 
 	if (array == NULL)
 		return (NULL);
 
 	for (i = min; i <= max; i++)
 	{
-		if (i != max)
-			array[i] = i;
+		array[i] = min + i;
 	}
 return (array);
 }
