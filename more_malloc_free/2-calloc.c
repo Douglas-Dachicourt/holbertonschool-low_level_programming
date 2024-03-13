53c1e2cd9e90dd295c61a61c12b598cb92009ebe
Douglas Dachicourt <8815@holbertonstudents.com>
2024-03-13 08:07:19 +0000

    ajout file 2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - fct qui alloue de la mémoire pour un tableau
+ * @nmemb: nombre d'éléments
+ * @size: taille de chaque élément
+ * Return: pointeur vers l'espace mémoire créé
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	unsigned int *array;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+
+	array = malloc(nmemb * size);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i <= (size); i++)
+		array[i] = 0;
+
+	return (array);
+}
