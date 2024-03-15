af7c5ee3732fad592837f9a1c27ebf3044a9ab50
Douglas Dachicourt <8815@holbertonstudents.com>
2024-03-15 12:43:37 +0000

    try

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,44 +2,36 @@
 #include <stdlib.h>
 #include "1-strdup.c"
 /**
- * new_dog -
- * @name:
- * @age:
- * @owner:
+ * new_dog - nouveau chien où l'on copie simplement le nom et le owner
+ * @name: le nom a passer en parametre
+ * @age: l'age a passer en parametre
+ * @owner: le proprietaire à passer en parametre
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	/*je définis un nouvel espace*/
 	dog_t *chien;
 
-/*j'alloue l'espace pour mon nouveau chien */
 	chien = malloc(sizeof(dog_t));
 
-/*si cet espace est correctement alloué*/
-	if (chien != NULL)
-	{
-		/*je copie dans mon chien le nom présent lors de l'appel*/
-		chien->name = _strdup(name);
-
-/*si je n'ai pas de name, je free le membre concerné et la structure en général*/
-		if (chien->name == NULL)
-		{
-			free(chien);
-			return (NULL);
-		}
-
-		chien->owner = _strdup(owner);
-
-		if (chien->owner == NULL)
-		{
-			free(chien->name);
-			free(chien);
-			return (NULL);
-		}
-
-		chien->age = age;
-	} else
+	if (chien == NULL)
 		return (NULL);
+
+	chien->name = _strdup(name);
+	if (chien->name == NULL)
+	{
+		free(chien);
+		return (NULL);
+	}
+
+	chien->owner = _strdup(owner);
+	if (chien->owner == NULL)
+	{
+		free(chien->name);
+		free(chien);
+		return (NULL);
+	}
+
+	chien->age = age;
 
 return (chien);
 }
