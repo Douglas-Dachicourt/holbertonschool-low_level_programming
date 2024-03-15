f9b05f28fd3ca12b3a455b8a3052fae34d2a298e
Douglas Dachicourt <8815@holbertonstudents.com>
2024-03-15 10:27:26 +0000

    try

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,16 +15,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (chien != NULL)
 	{
-		if(chien->name != NULL)
+		chien->name = _strdup(name);
+
+		if (chien->name == NULL)
 		{
-			chien->name = _strdup(name);
+			free(chien->name);
+			free(chien);
+			return (NULL);
 		}
 
 		chien->age = age;
 
-		if (chien->owner != NULL)
+		if (chien->owner == NULL)
 		{
-			chien->owner = _strdup(owner);
+			free(chien->owner);
+			free(chien);
+			return (NULL);
 		}
 	} else
 		return (NULL);
