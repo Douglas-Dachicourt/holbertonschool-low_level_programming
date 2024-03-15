2d34e708f2ef7702f882fc79a03d1c108fb5070d
Douglas Dachicourt <8815@holbertonstudents.com>
2024-03-15 14:30:20 +0000

    try

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -19,5 +19,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void free_dog(dog_t *d);
 char *_strdup(char *str);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
