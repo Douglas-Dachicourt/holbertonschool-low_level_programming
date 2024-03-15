1e2e68c9c7c8e30ee3f3370a0e9ff4a1590ce3a4
Douglas Dachicourt <8815@holbertonstudents.com>
2024-03-15 09:43:05 +0000

    try

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -6,10 +6,16 @@
  */
 void free_dog(dog_t *d)
 {
-	if (d != NULL)
+	if (d->name != NULL)
 	{
 		free(d->name);
+	}
+	if (d->age != NULL)
+	{
 		free(d->age);
+	}
+	if (d->owner != NULL)
+	{
 		free(d->owner);
 	}
 	free(d);
