dc1c775d81b8ce3ec8ab994b21be187a1158bc93
Douglas Dachicourt <8815@holbertonstudents.com>
2024-03-18 14:13:10 +0000

    ajout dossier funct pointers + 1er fichier

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+/**
+ * print_name - fct qui imprime la cdc souhaitée via une autre fct
+ * @name: la cdc que l'on souhaite passer
+ * @f: la fonction que l'on souhaite appeler
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
