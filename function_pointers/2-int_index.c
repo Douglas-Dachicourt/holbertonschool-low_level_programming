8b4b38a8e300439fecbdf3131aecc676bca7de43
Douglas Dachicourt <8815@holbertonstudents.com>
2024-03-18 15:08:54 +0000

    ajout file 2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * int_index - fct qui passe en prmt une autre fct qui chk les int
+ * @array: tableau d'int à check
+ * @size: la taille du tableau
+ * @cmp: la fct prmtr qui compare les int
+ * Return: -1 si aucun élément ne match; l'index si l'élément match
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; array[i] < size; i++)
+	{
+		cmp(array[i]);
+	}
+	return (i);
+}
