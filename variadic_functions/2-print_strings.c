da32f3f4c747ae40f3ca5654b17988a0aff086c6
Douglas Dachicourt <8815@holbertonstudents.com>
2024-03-20 10:02:51 +0000

    ajout file 2

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_strings - fct qui imprime des cdc en fct d'un séparateur et de son nb
+ * @separator: séparateur à afficher (si non nul)
+ * @n: le nb de cdc
+ * Return: nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%s", va_arg(args, char *));
+
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+va_end(args);
+printf("\n");
+}
