2e451495adca79ad5ec65aed1cfd1fe85da7051c
Douglas Dachicourt <8815@holbertonstudents.com>
2024-03-20 16:41:22 +0000

    try

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -6,10 +6,23 @@
  * @format: liste des arguments passés à la fonction
  * Return: nothing
  */
+struct list_types
+{
+	char type;
+	union
+	{
+	char c;
+	int num;
+	float f;
+	char *s;
+	} value;
+};
+
 void print_all(const char * const format, ...)
 {
 	va_list args;
-
 	va_start(args, format);
+
+
 
 }
