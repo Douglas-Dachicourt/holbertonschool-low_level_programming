6c40690c42364e8d37281d1a3acf993e7e886534
Douglas Dachicourt <8815@holbertonstudents.com>
2024-03-21 18:22:24 +0000

    try

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,40 +1,46 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
-
+/**
+ * print_all - fct qui imprime les argmts en fct de leur type de format
+ * @format: le format spécifié à respecter
+ */
 void print_all(const char * const format, ...)
 {
 	const char *temp = format;
 
 	va_list args;
+
 	va_start(args, format);
 
-	while (*temp != '\0')
+	while (*temp)
 	{
 		if (*temp == 'i')
 		{
 			int i = va_arg(args, int);
+
 			printf("%d", i);
 
 		} else if (*temp == 'c')
 		{
 			char c = (char)va_arg(args, int);
+
 			printf("%c", c);
 
 		} else if (*temp == 's')
 		{
 			char *s = va_arg(args, char *);
-			printf("%s", s);
+
+			if (s != NULL)
+				printf("%s", s);
 
 		} else if (*temp == 'f')
 		{
 			float f = (float)va_arg(args, double);
+
 			printf("%f", f);
 		}
 		temp++;
-	if (*temp != '\0' && (*temp == 'i' || *temp == 'c' || *temp == 's' ||
-	*temp == 'f'))
-		printf(", ");
 	}
 	va_end(args);
 	printf("\n");
--- variadic_functions/main.c
+++ variadic_functions/main.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-    print_all("fcsi", 3.14435, 'H', "#Cisfun", 0);
-    return (0);
+	print_all("mnbvfqcepolsbxzi", 3.14435, 'H', "#Cisfun", 0);
+	return (0);
 }
 
