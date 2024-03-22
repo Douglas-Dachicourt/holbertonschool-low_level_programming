8b716649abe05af838a36807f74280a25dfee38e
Douglas Dachicourt <8815@holbertonstudents.com>
2024-03-22 09:17:29 +0000

    try

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,50 +1,60 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-void p_c(char c) {
-    printf("%c, ", c);
+void p_i(int i)
+{
+	printf("%d", i);
 }
 
-void p_i(int i) {
-    printf("%d", i);
+void p_c(char c)
+{
+	printf("%c, ", c);
 }
 
-void p_f(float f) {
-    printf("%f, ", f);
+void p_f(float f)
+{
+	printf("%f", f);
 }
 
-void p_s(char *s) {
-    if (s == NULL) {
-        printf("(nil)");
-    } else {
-        printf("%s, ", s);
-    }
+void p_s(char *s)
+{
+	if (s != NULL)
+	{
+		printf("%s, ", s);
+	} else
+		printf("(nil)");
 }
 
-void print_all(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
+void print_all(const char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
 
-    while (*format) {
-        switch (*format) {
-            case 'c':
-                p_c(va_arg(args, int));
-                break;
-            case 'i':
-                p_i(va_arg(args, int));
-                break;
-            case 'f':
-                p_f(va_arg(args, double));
-                break;
-            case 's':
-                p_s(va_arg(args, char *));
-                break;
-            default:
-                break;
-        }
-        format++;
-    }
+	while (*format != '\0')
+	{
+		switch (*format)
+		{
+			case 'c':
+			p_c(va_arg(args, int));
+			break;
 
-    va_end(args);
-    printf("\n");
+			case 'i':
+			p_i(va_arg(args, int));
+			break;
+
+			case 'f':
+			p_f(va_arg(args, double));
+			break;
+
+			case 's':
+			p_s(va_arg(args, char *));
+			break;
+
+			default:
+			break;
+		}
+		format++;
+	}
+	va_end(args);
+	printf("\n");
 }
