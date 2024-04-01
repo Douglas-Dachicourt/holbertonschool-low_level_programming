ff5c4bb7b7312d2d6e7e4c956a030db210fc9ef5
Douglas Dachicourt <8815@holbertonstudents.com>
2024-04-01 15:24:07 +0000

    new attempt

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,12 +1,24 @@
 #include "lists.h"
-
+/**
+ * print_list - fct which print out all the elem of a list
+ * @h: the list of elements
+ * Return: number of nodes
+ */
 size_t print_list(const list_t *h)
 {
-	if (h == NULL)
+	size_t count = 0;
+
+	while (h != NULL)
 	{
-		printf("[0] (nil)\n");
-		return (0);
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		} else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		count++;
+		h = h->next;
 	}
-	printf("[%d] %s\n",h -> len, h -> str);
-    return (1 + print_list(h -> next));
+return (count);
 }
