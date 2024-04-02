6a6ab2d195c18c4484f6051b1f255e713c6b73bf
Douglas Dachicourt <8815@holbertonstudents.com>
2024-04-02 13:10:45 +0000

    add file 1

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * dlistint_len - fct which return number of elem in a double linked list
+ * @h: list of elem
+ *
+ * Return: number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->n != 0)
+			count++;
+
+		h = h->next;
+	}
+	return (count);
+}
