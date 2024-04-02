d80c45a1db46b4552aca11219a176123aa7a1d49
Douglas Dachicourt <8815@holbertonstudents.com>
2024-04-02 11:27:15 +0000

    try

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+/**
+ * add_node_end - fct which add a node on the end ot he lkded list
+ * @head: head node of list
+ * @str: string to copy
+ * Return: new_node is the adress of the new node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node = (list_t *) malloc(sizeof(list_t));
+
+	int i = 0;
+	int length = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+		length++;
+	}
+		new_node->str = strdup(str);
+		new_node->len = length;
+		new_node->next = NULL;
+
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		list_t *last = *head;
+
+		while (last->next != NULL)
+		{
+			last = last->next;
+		}
+
+	last->next = new_node;
+
+	}
+
+	return (new_node);
+}
