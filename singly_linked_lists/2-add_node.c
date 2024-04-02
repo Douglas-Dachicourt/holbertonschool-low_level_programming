76a976a82a1fb16e93f0a4af23fd75d9f16539ab
Douglas Dachicourt <8815@holbertonstudents.com>
2024-04-02 08:30:51 +0000

    add file 2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * add_node - fct which add a node on the head ot he lkded list
+ * @head: head node of list
+ * @str: string to copy
+ * Return : new_node the adress of the new node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node = (list_t *) malloc(sizeof(list_t *));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = *head;
+
+	*head = new_node;
+
+	return (new_node);
+}
