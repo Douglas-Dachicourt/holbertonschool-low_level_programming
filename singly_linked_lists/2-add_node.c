f874327cbf803fe898d0032388e548daa19ff19d
Douglas Dachicourt <8815@holbertonstudents.com>
2024-04-02 09:01:45 +0000

    try

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -7,12 +7,21 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = (list_t *) malloc(sizeof(list_t *));
+	list_t *new_node = (list_t *) malloc(sizeof(list_t));
+	int i = 0;
+	int length = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+		length++;
+	}
 
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->str = strdup(str);
+	new_node->len = length;
 
 	if (new_node->str == NULL)
 	{
