b254b17bdd5218b08efec1bc72e610e08113437d
Douglas Dachicourt <8815@holbertonstudents.com>
2024-04-02 08:53:36 +0000

    try

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -3,7 +3,7 @@
  * add_node - fct which add a node on the head ot he lkded list
  * @head: head node of list
  * @str: string to copy
- * Return : new_node the adress of the new node
+ * Return: new_node the adress of the new node
  */
 list_t *add_node(list_t **head, const char *str)
 {
@@ -13,7 +13,6 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	new_node->str = strdup(str);
-	new_node->len = strlen(str);
 
 	if (new_node->str == NULL)
 	{
