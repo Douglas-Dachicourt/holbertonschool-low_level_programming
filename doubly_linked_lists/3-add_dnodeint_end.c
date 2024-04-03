c1798e0d7b5b0b1c5efb80246347e0b8ab527c15
Douglas Dachicourt <8815@holbertonstudents.com>
2024-04-03 08:12:28 +0000

    try

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,15 +10,14 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *new_node = (dlistint_t *) malloc(sizeof(dlistint_t));
 
 	if (new_node == NULL)
-	{
-		free(new_node);
 		return (NULL);
-	}
+
 	new_node->n = n;
 	new_node->next = NULL;
 
 	if (*head == NULL)
 	{
+		new_node->prev = NULL;
 		*head = new_node;
 	}
 	else
@@ -30,6 +29,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 			last = last->next;
 		}
 	last->next = new_node;
+	new_node->prev = last;
 	}
 	return (new_node);
 }
