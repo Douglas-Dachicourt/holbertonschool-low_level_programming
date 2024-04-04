8e599b218d4d8f9c73006e9392de95c9df028c93
Douglas Dachicourt <8815@holbertonstudents.com>
2024-04-04 15:02:56 +0000

    adding of new project folder + first file 0

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * binary_to_uint - fct which convert binary into dec int
+ * @b: the string of char to convert
+ * Return: the decimal number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i = 0;
+	unsigned int num = 0;
+	unsigned int dec = 0;
+
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		num = b[i] - '0';
+		dec = dec * 2 + (num);
+		i++;
+	}
+	return (dec);
+}
