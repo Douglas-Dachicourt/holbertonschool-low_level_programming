7319ffc071d8fce916798858716dd5e031b56df1
Douglas Dachicourt <8815@holbertonstudents.com>
2024-04-05 13:20:20 +0000

    try on file 2

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -3,25 +3,14 @@
  * get_bit - fct which give the value of a bit with a known index
  * @n: number to test
  * @index: index of the value to find
- * Return:
+ * Return: value of the bit a the specified index
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int bit_size = sizeof((unsigned long int) n);
-	unsigned long int i;
+	unsigned long int bit_size = sizeof(unsigned long int) * 8;
 
-	if (index == 0)
-		return (0);
-
-	if (!index)
+	if (index >= bit_size)
 		return (-1);
 
-	for (i = 0; i < bit_size - 1; i++)
-	{
-		if (index == i)
-		{
-			return (i);
-		}
-	}
-	return (1);
+	return (n >> index & 1);
 }
