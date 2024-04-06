a4d1f1fa82337244b9e0927fb6173d4bb8e60943
Douglas Dachicourt <8815@holbertonstudents.com>
2024-04-06 13:58:16 +0000

    try

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -26,6 +26,9 @@ int create_file(const char *filename, char *text_content)
 	if (create == -1)
 		return (-1);
 
+	if (text_content == NULL)
+		return (1);
+
 	wr = write(create, text_content, len);
 	if (wr == -1)
 	{
