807d6d91f2a472ef35ddd994d0596cc58026e059
Douglas Dachicourt <8815@holbertonstudents.com>
2024-04-07 09:38:04 +0000

    add file 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -12,7 +12,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	int ouvrir, ecrire;
 
 	if (!filename)
-		return (-1);
+		return (-1)
+	return (1);
 
 	if (text_content == NULL)
 		return (-1);
