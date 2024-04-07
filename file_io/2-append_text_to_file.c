7efcd26d20ea95a857eaa36501eb4736ce0059bc
Douglas Dachicourt <8815@holbertonstudents.com>
2024-04-07 10:01:27 +0000

    add file 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-		return (-1);
+		return (1);
 	while (text_content[i] != '\0')
 	{
 		i++;
@@ -24,13 +24,16 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	ouvrir = open(filename, O_RDWR | O_APPEND);
 	if (ouvrir == -1)
-		return (1);
+		return (-1);
 
 	if (text_content != NULL)
 	{
 		ecrire = write(ouvrir, text_content, len);
 		if (ecrire == -1)
+		{
+			close(ouvrir);
 			return (0);
+		}
 	}
 	close(ouvrir);
 
